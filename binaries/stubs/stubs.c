df8a7a5d2735eed0a9f7fc599fdda80d634d2e05
Geeoon Chung <geeoon13@gmail.com>
2026-04-11 18:13:18 -0700

    Added stubs for unicorn hooks

---
+++ binaries/stubs/stubs.c
@@ -0,0 +1,57 @@
+#include <sys/stat.h>
+#include <errno.h>
+
+#define EXIT_ADDR (int*)0x10000
+#define READ_ADDR (char*)0x10010
+#define WRITE_ADDR (char*)0x10020
+
+// unicorn hook
+void _exit(int status) {
+    *EXIT_ADDR = status;
+    while(1);
+}
+
+int _read(int fd, char *buf, int len) {
+    for (int i = 0; i < len; i++) buf[i] = *READ_ADDR;
+    return len;
+}
+
+int _write(int fd, char *buf, int len) {
+    for (int i = 0; i < len; i++) *WRITE_ADDR = buf[i];
+    return len;  // pretend we wrote it, unicorn hook will intercept
+}
+
+// should NOT be used
+int _lseek(int fd, int offset, int whence) {
+    return -1;
+}
+
+// should NOT be used
+int _close(int fd) {
+    return -1;
+}
+
+// should NOT be used
+void *_sbrk(int incr) {
+    return (void *)-1;
+}
+
+// should NOT be used
+int _isatty(int fd) {
+    return -1;
+}
+
+// should NOT be used
+int _kill(int pid, int sig) {
+    return -1;
+}
+
+// should NOT be used
+int _getpid(void) {
+    return -1;
+}
+
+// should NOT be used
+int _fstat(int fd, struct stat *st) {
+    return -1;
+}
