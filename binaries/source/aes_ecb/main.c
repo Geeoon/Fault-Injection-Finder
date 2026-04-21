00fc42e0ded2b78f456151fbc5d1d5ce26c5a4c1
Geeoon <geeoon13@gmail.com>
2026-04-21 15:26:37 -0700

    Added AES binary

---
+++ binaries/source/aes_ecb/main.c
@@ -0,0 +1,21 @@
+#include "AES_256_ECB.h"
+
+extern int _read(int fd, char* buf, int len);
+
+int main(void) {
+    AES_CTX ctx;
+    const unsigned char key[AES_KEY_SIZE] = { 0xe7, 0x7d, 0xbb, 0xa2, 0x30, 0x9c, 0xdc, 0xa3 };
+    unsigned char data[AES_BLOCK_SIZE];
+    
+    // get data
+    int n = _read(0, data, AES_BLOCK_SIZE);
+
+    // encrypt data
+    AES_EncryptInit(&ctx, key);
+    AES_Encrypt(&ctx, data, data);
+
+    // decrypt data
+    AES_DecryptInit(&ctx, key);
+    AES_Decrypt(&ctx, data, data);
+    return 0;
+}
