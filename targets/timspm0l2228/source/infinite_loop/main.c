868446ec2308935f7b7a84a16fb4596601cfecb0
Geeoon <geeoon13@gmail.com>
2026-05-15 13:08:16 -0700

    Added infinite loop binary

---
+++ targets/timspm0l2228/source/infinite_loop/main.c
@@ -0,0 +1,13 @@
+#include "string.h"
+
+extern void init_device(void);
+extern int _write(int fd, char* buf, int len);
+
+int main() {
+    // prevents gcc from optimizing away the stuff aster the loop
+    init_device();
+    volatile int dummy = 1;
+    while (dummy) {}
+    _write(0, "escaped the loop", strlen("escaped the loop"));
+    return 0;
+}
