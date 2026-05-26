cc9e571cb6d6c6a0d669b9d51d32ac4bfe90bb13
Geeoon <geeoon13@gmail.com>
2026-05-26 00:40:35 -0700

    Added aes test

---
+++ targets/timspm0l2228/source/aes_ecb/main.c
@@ -0,0 +1,28 @@
+#include "AES_256_ECB.h"
+
+extern void init_device(void);
+extern int _read(int fd, char* buf, int len);
+extern void pwned(void);
+
+int main(void) {
+    init_device();
+    volatile int dummy = 0;
+    if (dummy) pwned(); 
+    
+    AES_CTX ctx;
+    const unsigned char key[AES_KEY_SIZE] = { 0xe7, 0x7d, 0xbb, 0xa2, 0x30, 0x9c, 0xdc, 0xa3 };
+    unsigned char data[AES_BLOCK_SIZE];
+    
+    // get data
+    int n = _read(0, data, AES_BLOCK_SIZE);
+
+    // encrypt data
+    AES_EncryptInit(&ctx, key);
+    AES_Encrypt(&ctx, data, data);
+
+    // decrypt data
+    AES_DecryptInit(&ctx, key);
+    AES_Decrypt(&ctx, data, data);
+
+    return 0;
+}
