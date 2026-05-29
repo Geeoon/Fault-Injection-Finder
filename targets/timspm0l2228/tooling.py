4318faaa65e80e235852dc5995b3848c7fc5d68c
Geeoon Chung <geeoon13@gmail.com>
2026-05-29 16:36:48 -0700

    More features for FPGA communicatoin

--- targets/timspm0l2228/tooling.py
+++ targets/timspm0l2228/tooling.py
@@ -10,7 +10,7 @@ from pyocd.core.helpers import ConnectHelper
 TRIALS = 10  # number of trials to do per glitch parameter
 
 class FPGAParameters():
-    def __init__(self, port: str, speed: int=115200, delay: int=0, length: int=10):
+    def __init__(self, port: str, speed: int=115200, delay: int=0, length: int=5):
         """
         Initializes an FPGAParameters object
         :param delay: how long to delay after a trigger before the glitch, in FPGA clock cycles
@@ -22,13 +22,15 @@ class FPGAParameters():
         self.delay = delay
         self.length = length
         self.ser = serial.Serial(port, speed, timeout=1)
+        self.delay_delta = 0
+        self.length_delta = 0
 
     def serialize(self) -> bytes:
         """
         Serializes the FPGAParameters to be sent over UART
         """
         # send the delay, then the length, then reset it
-        return ((0b10101010).to_bytes(1), b'\0' + struct.pack(">I", self.delay),  b'\xFF' + struct.pack(">B", self.length))
+        return ((0b10101010).to_bytes(1), b'\0' + struct.pack(">I", self.get_delay()),  b'\xFF' + struct.pack(">B", self.get_length()))
 
     def send(self):
         """
@@ -41,6 +43,38 @@ class FPGAParameters():
             self.logger.info(f"Got {res} from the FPGA")
             if not res:
                 raise Exception("FPGA timed out")
+    
+    def set_delay_delta(self, delta: int=0):
+        """
+        Sets the delta for the delay
+        :param delta: the new delta
+        """
+        if self.delay + delta < 0:
+            raise ValueError("Delta cannot be less than the current delay")
+        self.delay_delta = delta
+
+    def set_length_delta(self, delta: int=0):
+        """
+        Sets the delta for the length of the glitch
+        :param delta:
+        """
+        if self.length + delta < 0:
+            raise ValueError("Delta cannot be less than the current glitch length")
+        self.length_delta = delta
+
+    def get_delay(self) -> int:
+        """
+        Get the current delay, including the delta
+        :return: delay + delta
+        """
+        return self.delay + self.delay_delta
+
+    def get_length(self) -> int:
+        """
+        Get the current length, including the delta
+        :return: length + delta
+        """
+        return self.length + self.length_delta
 
     def close(self):
         """
@@ -64,7 +98,6 @@ class TargetDevice():
         Sets the function to be ran for setting up a glitch
         :param setup: the setup function to be ran
         """
-        self.logger.debug("Running the setup callback")
         self.setup = setup
 
     def run(self):
@@ -74,7 +107,7 @@ class TargetDevice():
         """
         self.logger.info("Running the program")
         self.setup(self.ser)
-        time.sleep(.01)
+        time.sleep(.05)
         real_output = self.ser.read_all()
         self.logger.info(f"Got the following output {real_output}")
         return self.expected_output in real_output
@@ -121,17 +154,28 @@ target = TargetDevice(expected_output=expected, port='/dev/ttyACM0', speed=11520
 target.set_setup_callback(setup_device)
 # parameters = FPGAParameters(port='/dev/ttyACM1', speed=115200)  # TODO: change to be the actual FPGA port
 
-# send the parameters to the FPGA
-for delay_delta in range(-10, 11):  # -10 to 10, going by 1
-    for length in range(8, 13, 2):  # 8 to 12, jumping by 2
-        # parameters.send()
-        for _ in range(TRIALS):
-            if target.run():
-                pass
-                # print(f"Successful fault with {self.parameters.delay} delay cycles with {self.parameters.length} glitching length cycles")
+successes = []
+try:
+    # send the parameters to the FPGA
+    for delay_delta in range(-10, 11):  # -10 to 10, going by 1
+        for length in range(-2, 3, 2):  # -2 to 2, going by 1
+            # parameters.set_delay_delta(delay_delta)
+            # parameters.set_length_delta(length)
+            # logging.info(f"Trying {TRIALS} attempts with {parameters.get_delay()} delay and {parameters.get_length()} length")
+            for _ in range(TRIALS):
+                # parameters.send()
+                if target.run():
+                    pass
+                    # logging.critical(f"Successful fault with {parameters.get_delay()} delay cycles with {parameters.get_length()} glitching length cycles")
+                    # successes.append({ "delay": parameters.get_delay(), "length": parameters.get_length() })
+except KeyboardInterrupt:
+    logging.info("Ending tool.")
+finally:
+    # Close the serial when done
+    # parameters.close()
+    target.close()
+    SESSION.close()
 
-# Close the serial when done
-# parameters.close()
-target.close()
-
-SESSION.close()
+logging.info("Finished run, results:")
+for success in successes:
+    logging.critical(f"Delay: {success['delay']}\nLength: {success['length']}")
