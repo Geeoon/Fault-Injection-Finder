5c15953a04ad3949909e5eb32cb66a47783ccfae
Geeoon <geeoon13@gmail.com>
2026-08-31 21:59:14 -0700

    Added note to password binary in rust

--- binaries/rust/source/password/src/main.rs
+++ binaries/rust/source/password/src/main.rs
@@ -6,10 +6,12 @@ use core::panic::PanicInfo;
 #[allow(unreachable_code)]
 #[no_mangle]
 pub extern "C" fn main() -> u32 {
-    const PASSWORD: [u8; 11] = *b"password123";
-    let mut input: [u8; 11] = [0; 11];
+    // NOTE: the length of our input actually makes a difference in whether the PC is fully or partially controllable
+    // with 11 bytes, we were only able to control the bottom 4 bytes and the top were held 0.  With 16, we get total control
+    const PASSWORD: [u8; 16] = *b"password12345678";
+    let mut input: [u8; 16] = [0; 16];
     loop {
-        stubs::_read(&mut input[0..11]);
+        stubs::_read(&mut input[0..16]);
         if PASSWORD == input {
             break;
         };
